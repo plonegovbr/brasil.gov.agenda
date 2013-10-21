8e5ca48fb46324f80d275c5bdeb7cc53eb51e584
Érico Andrei <erico@simplesconsultoria.com.br>
2013-10-21 11:54:43 -0200

    Altera a maneira de setarmos o valor padrao para o behavior

---
+++ src/brasil/gov/agenda/subscribers/agenda.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from brasil.gov.agenda.interfaces import IAgenda
+from brasil.gov.agenda.config import PROJECTNAME
+from five import grok
+from plone.app.dexterity.behaviors.nextprevious import INextPreviousToggle
+from zope.lifecycleevent.interfaces import IObjectAddedEvent
+import logging
+
+
+logger = logging.getLogger(PROJECTNAME)
+
+
+@grok.subscribe(IObjectAddedEvent)
+def habilita_next_previous(event, obj=None):
+    """ Ordenacao por id dentro de uma agenda
+    """
+    if not obj:
+        obj = event.object
+
+    if not IAgenda.providedBy(obj):  # nao eh uma agenda
+        return
+
+    behavior = INextPreviousToggle(obj)
+    behavior.nextPreviousEnabled = True
