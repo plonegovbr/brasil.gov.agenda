0c7c6777c44b82c2fbdf30b4866430295d03609f
Érico Andrei <erico@simplesconsultoria.com.br>
2014-02-16 23:43:20 -0300

    Oculta upgrades

--- src/brasil/gov/agenda/config.py
+++ src/brasil/gov/agenda/config.py
@@ -1,10 +1,24 @@
 # -*- coding:utf-8 -*-
 from Products.CMFPlone import interfaces as plone_interfaces
+from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
 from zope.interface import implements
 
 PROJECTNAME = "brasil.gov.agenda"
 
 AGENDADIARIAFMT = '%Y-%m-%d'
+
+
+class HiddenProducts(object):
+
+    implements(qi_interfaces.INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return [
+            'brasil.gov.agenda.upgrades.v2000'
+            'brasil.gov.agenda.upgrades.v3000'
+            'brasil.gov.agenda.upgrades.v4000'
+            'brasil.gov.agenda.upgrades.v4001'
+        ]
 
 
 class HiddenProfiles(object):
@@ -14,6 +28,8 @@ class HiddenProfiles(object):
         return [
             u'brasil.gov.agenda:uninstall',
             u'brasil.gov.agenda.upgrades.v2000:default'
+            u'brasil.gov.agenda.upgrades.v3000:default'
+            u'brasil.gov.agenda.upgrades.v4000:default'
             u'collective.portlet.calendar:default'
             u'collective.portlet.calendar:uninstall'
         ]
