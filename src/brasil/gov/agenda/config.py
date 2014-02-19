2049b1eed902cf7e03ff164f2a50ee8088328b6a
Danilo Barbato <dbarbato@tv1.com.br>
2014-02-19 14:48:44 -0300

    Acerto ocultando upgrade steps

--- src/brasil/gov/agenda/config.py
+++ src/brasil/gov/agenda/config.py
@@ -9,14 +9,13 @@ AGENDADIARIAFMT = '%Y-%m-%d'
 
 
 class HiddenProducts(object):
-
     implements(qi_interfaces.INonInstallable)
 
     def getNonInstallableProducts(self):
         return [
-            'brasil.gov.agenda.upgrades.v2000'
-            'brasil.gov.agenda.upgrades.v3000'
-            'brasil.gov.agenda.upgrades.v4000'
+            'brasil.gov.agenda.upgrades.v2000',
+            'brasil.gov.agenda.upgrades.v3000',
+            'brasil.gov.agenda.upgrades.v4000',
             'brasil.gov.agenda.upgrades.v4001'
         ]
 
@@ -27,9 +26,9 @@ class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
         return [
             u'brasil.gov.agenda:uninstall',
-            u'brasil.gov.agenda.upgrades.v2000:default'
-            u'brasil.gov.agenda.upgrades.v3000:default'
-            u'brasil.gov.agenda.upgrades.v4000:default'
-            u'collective.portlet.calendar:default'
+            u'brasil.gov.agenda.upgrades.v2000:default',
+            u'brasil.gov.agenda.upgrades.v3000:default',
+            u'brasil.gov.agenda.upgrades.v4000:default',
+            u'collective.portlet.calendar:default',
             u'collective.portlet.calendar:uninstall'
         ]
