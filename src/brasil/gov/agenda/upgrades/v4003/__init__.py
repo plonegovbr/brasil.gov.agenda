6eb4eabd87772319936c539f90f5497601a1c639
hvelarde <hector.velarde@gmail.com>
2014-12-04 14:23:54 -0200

    Refactor upgrade step and add test

--- src/brasil/gov/agenda/upgrades/v4003/__init__.py
+++ src/brasil/gov/agenda/upgrades/v4003/__init__.py
@@ -11,3 +11,13 @@ def make_agenda_linkable_on_tinymce(context):
         linkable.append('Agenda')
         tinymce.linkable = u'\n'.join(linkable)
         logger.info('Agenda added to linkable types in TinyMCE.')
+
+
+def update_effective_date_index(context):
+    """Reindex existing AgendaDiaria objects to fix EffectiveDate metadata."""
+    catalog = api.portal.get_tool('portal_catalog')
+    results = catalog(portal_type='AgendaDiaria')
+    for brain in results:
+        o = brain.getObject()
+        o.reindexObject(idxs=['EffectiveDate'])
+    logger.info('{:d} objects reindexed'.format(len(results)))
