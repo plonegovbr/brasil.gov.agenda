df733abf25badd27dc0a6b72982d7f9307b8c096
Héctor Velarde <hector.velarde@gmail.com>
2014-12-05 17:23:30 -0200

    Fix trove classifiers

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description=description,
     long_description=long_description,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Plone :: 4.3',
