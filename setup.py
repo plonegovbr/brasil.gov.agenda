86ca02c43b6885e3044fc94f87b73d2f6ad88964
hvelarde <hector.velarde@gmail.com>
2017-11-08 19:45:40 -0200

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.1.1'
+version = '1.1.2.dev0'
 description = 'Agenda de membros do Governo Brasileiro'
 long_description = (
     open('README.rst').read() + '\n' +
