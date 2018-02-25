2b5a48f418ec04407a98b7ddc14bcf178ec68d26
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-02-24 22:58:50 -0300

    Fix exception when run upgrade step two times

--- src/brasil/gov/agenda/upgrades/v4004/__init__.py
+++ src/brasil/gov/agenda/upgrades/v4004/__init__.py
@@ -10,21 +10,23 @@ NEW_CSS = '++resource++brasil.gov.agenda/brasilgovagenda.css'
 NEW_JS = '++resource++brasil.gov.agenda/brasilgovagenda.js'
 
 
+def add_resource(tool, resource_id):
+    if resource_id in tool.getResourceIds():
+        return
+    tool.registerResource(resource_id)
+    tool.getResource(resource_id).setCompression('none')
+
+
 def fix_resources_references(setup_tool):
     """Fix resource references."""
     css_tool = api.portal.get_tool('portal_css')
-    css_tool.registerResource(SWIPER_CSS)
-    res = css_tool.getResource(SWIPER_CSS)
-    res.setCompression('none')
-    css_tool.moveResourceBefore(res, '*')
+    add_resource(css_tool, SWIPER_CSS)
+    css_tool.moveResourceBefore(SWIPER_CSS, '*')
     if OLD_CSS in css_tool.getResourceIds():
         css_tool.getResource(OLD_CSS).setCompression('none')
         css_tool.renameResource(OLD_CSS, NEW_CSS)
     js_tool = api.portal.get_tool('portal_javascripts')
-    js_tool.registerResource(SWIPER_JS)
-    res = js_tool.getResource(SWIPER_JS)
-    res.setCompression('none')
-    js_tool.moveResourceBefore(res, '*')
-    js_tool.registerResource(NEW_JS)
-    js_tool.getResource(NEW_JS).setCompression('none')
+    add_resource(js_tool, SWIPER_JS)
+    js_tool.moveResourceBefore(SWIPER_JS, '*')
+    add_resource(js_tool, NEW_JS)
     logger.info('Updated static resource references.')
