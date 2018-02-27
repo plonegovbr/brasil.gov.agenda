c0d20945a0f5b236ad73d37fb8e3a6c4fa505af1
hvelarde <hector.velarde@gmail.com>
2018-02-27 11:25:18 -0300

    Do not register subscribers using five.grok

--- src/brasil/gov/agenda/subscribers/agenda.py
+++ src/brasil/gov/agenda/subscribers/agenda.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-
-from brasil.gov.agenda.config import PROJECTNAME
-from brasil.gov.agenda.interfaces import IAgenda
-from five import grok
 from plone.app.dexterity.behaviors.nextprevious import INextPreviousToggle
-from zope.lifecycleevent.interfaces import IObjectAddedEvent
-
-import logging
 
 
-logger = logging.getLogger(PROJECTNAME)
-
-
-@grok.subscribe(IObjectAddedEvent)
-def habilita_next_previous(event, obj=None):
-    """ Ordenacao por id dentro de uma agenda
-    """
-    if not obj:
-        obj = event.object
-
-    if not IAgenda.providedBy(obj):  # nao eh uma agenda
-        return
-
+def habilita_next_previous(obj, event):
+    """Ordenacao por id dentro de uma agenda."""
     behavior = INextPreviousToggle(obj)
     behavior.nextPreviousEnabled = True
--- src/brasil/gov/agenda/subscribers/agendadiaria.py
+++ src/brasil/gov/agenda/subscribers/agendadiaria.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
-
 from Acquisition import aq_parent
-from brasil.gov.agenda.config import PROJECTNAME
-from brasil.gov.agenda.interfaces import IAgendaDiaria
-from five import grok
-from zope.lifecycleevent.interfaces import IObjectAddedEvent
 
 import datetime
-import logging
 
 
-logger = logging.getLogger(PROJECTNAME)
-
-
-@grok.subscribe(IObjectAddedEvent)
-def ordenacao_agenda(event, obj=None):
-    """ Ordenacao por id dentro de uma agenda
-    """
-    if not obj:
-        obj = event.object
-
-    if not IAgendaDiaria.providedBy(obj):  # nao eh uma agenda
-        return
-
+def ordenacao_agenda(obj, event):
+    """Ordenacao por id dentro de uma agenda."""
     # Forcamos a data manualmente pois quando nao alterada
     # ele mantem o default_factory como valor padrao (o que muda todos os dias)
     date = [int(p) for p in obj.getId().split('-')]
