729fa7aa31d51809e6f5f01e02beb8a0b3d088c8
idgserpro <idgserpro@users.noreply.github.com>
2018-05-16 10:49:18 -0300

    Issue 82 erro portlet calendario idgserpro (#87)
    * Corrige layout do portlet de agenda (rel #82).
    
    portlet_calendar.js foi customizado. Ler o relato para entender as
    motivações.
    
    Customizado a partir de
    
    https://raw.githubusercontent.com/plone/plone.app.event/1.1.8/plone/app/event/portlets/portlet_calendar.js
    
    * Adiciona upgradeStep para customização js.
    
    O último upgradeStep da branch 1.x é o 4003:
    
    https://github.com/plonegovbr/brasil.gov.agenda/tree/dd4fcfa103d625a1ee7f44cba9b6457e2a6b6a17/src/brasil/gov/agenda/upgrades
    
    E o primeiro da branch master (2.0) é o 4004:
    
    https://github.com/plonegovbr/brasil.gov.agenda/tree/58370bbb9c5a9182c8979aba7669bb09c10d9b82/src/brasil/gov/agenda/upgrades
    
    Dessa forma, nomearemos o upgradeStep na branch 1.x de 4003r01 para evitar
    conflitos com nomes. Essa lógica é parecida com a usada nos revisions dos
    UnifiedInstallers:
    
    https://launchpad.net/plone/4.3/4.3.15/+download/Plone-4.3.15-UnifiedInstaller-r2.tgz
    
    E também é reconhecida pelos padrões definidos em distutils.version.LooseVersion
    e packaging.version.parse.
    
    * Corrige navegação entre meses calendário estendido
    
    Fecha https://github.com/plonegovbr/brasil.gov.agenda/issues/82.
    
    Patch foi feito da forma tradicional e não com uso de
    collective.monkeypatcher, ver
    
    https://pypi.org/project/collective.monkeypatcher/#patching-module-level-functions
    
    * Muda upgradeStep de 4003r01 para 4004.
    
    Após discussões sobre padrões de números de upgradeSteps em
    
    https://github.com/plonegovbr/brasil.gov.portal/pull/471#discussion_r187339865
    
    Decidimos mudar para 4004. Modificações futuras no brasil.gov.agenda com
    relação ao upgradeStep hoje definido na branch master (futuro 2.x) serão
    feitas em
    
    https://github.com/plonegovbr/brasil.gov.agenda/issues/86

--- src/brasil/gov/agenda/__init__.py
+++ src/brasil/gov/agenda/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from brasil.gov.agenda import patches
 from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('brasil.gov.agenda')
+
+
+patches.run()
---
+++ src/brasil/gov/agenda/portlets/overrides/portlet_calendar.js
@@ -0,0 +1,37 @@
+(function ($) {
+
+    function load_portlet_calendar(event, elem) {
+        // depends on plone_javascript_variables.js for portal_url
+        event.preventDefault();
+        var pw = elem.closest('.portletWrapper');
+        var elem_data = elem.data();
+        var portlethash = pw.attr('id');
+        portlethash = portlethash.substring(15, portlethash.length);
+        url = portal_url +
+              '/@@render-portlet?portlethash=' + portlethash +
+              '&year=' + elem_data.year +
+              '&month=' + elem_data.month;
+        $.ajax({
+            url: url,
+            success: function (data) {
+                pw.html(data);
+                rebind_portlet_calendar();
+            }
+        });
+    }
+
+    function rebind_portlet_calendar() {
+        // ajaxify each portletCalendar
+        $('.portletCalendar a.calendarNext').click(function (event) {
+            load_portlet_calendar(event, $(this));
+        });
+        $('.portletCalendar a.calendarPrevious').click(function (event) {
+            load_portlet_calendar(event, $(this));
+        });
+    }
+
+    $(document).ready(function () {
+        rebind_portlet_calendar();
+    });
+
+}(jQuery));
