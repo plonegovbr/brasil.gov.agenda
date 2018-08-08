12edc3104d83860032c6be220b1dfd5fedfd1856
hvelarde <hector.velarde@gmail.com>
2018-08-08 18:13:26 -0300

    Add tests for AgendaDiaria event subscriber

--- src/brasil/gov/agenda/tests/test_subscribers.py
+++ src/brasil/gov/agenda/tests/test_subscribers.py
@@ -22,3 +22,29 @@ class AgendaSubscriberTestCase(unittest.TestCase):
             container=self.folder, type='Agenda', id='agenda')
         self.assertTrue(INextPreviousToggle.providedBy(agenda))
         self.assertTrue(agenda.nextPreviousEnabled)
+
+
+class AgendaDiariaSubscriberTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+        with api.env.adopt_roles(['Manager']):
+            folder = api.content.create(
+                container=self.portal, type='Folder', id='test-folder')
+
+        self.agenda = api.content.create(
+            container=folder, type='Agenda', id='agenda')
+
+    def test_sort_order(self):
+        # create content in opposite order
+        for day in ('03', '02', '01'):
+            id_ = '2018-08-' + day
+            api.content.create(
+                container=self.agenda, type='AgendaDiaria', id=id_)
+
+        # check is ordered by id
+        expected = ['2018-08-01', '2018-08-02', '2018-08-03']
+        self.assertEqual(self.agenda.objectIds(), expected)
