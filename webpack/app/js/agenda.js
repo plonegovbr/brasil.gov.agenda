52b03af517d146eb79d4ec6fc31d4d43e10591f5
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-09-20 15:59:00 -0300

    Simplify edit / delete overlay

--- webpack/app/js/agenda.js
+++ webpack/app/js/agenda.js
@@ -9,36 +9,31 @@ export default class AgendaView {
     this.container = container;
     this.datepicker = new DatePicker(container);
     this.$appointments = this.$('.list-compromissos');
-    this.editable = container.dataset.editable === 'True';
-    if (this.editable) {
-      this.makeOverlayEdit();
-    }
+    this.bindEvents();
   }
   $(selector) {
     return $(selector, this.container.parentElement);
   }
-  makeOverlayEdit(){
-    for (let item of $('.item-compromisso-wrapper')) {
-      $('.editar_compromisso', item).prepOverlay({
-        subtype: 'ajax',
-        filter: common_content_filter,
-        formselector: '#form',
-        cssclass: 'overlay-compromisso',
-        noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
-        redirect: location.href,
-        closeselector: '[name="form.buttons.cancel"]',
-        width:'50%'
-      });
-      $('.remover_compromisso', item).prepOverlay({
-        subtype: 'ajax',
-        filter: common_content_filter,
-        formselector: '#delete_confirmation',
-        cssclass: 'overlay-compromisso',
-        noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
-        redirect: location.href,
-        closeselector: '[name="form.button.Cancel"]',
-        width:'50%'
-      });
-    }
+  bindEvents(){
+    this.$('.editar_compromisso').prepOverlay({
+      subtype: 'ajax',
+      filter: common_content_filter,
+      formselector: '#form',
+      cssclass: 'overlay-compromisso',
+      noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
+      redirect: location.href,
+      closeselector: '[name="form.buttons.cancel"]',
+      width:'50%'
+    });
+    this.$('.remover_compromisso').prepOverlay({
+      subtype: 'ajax',
+      filter: common_content_filter,
+      formselector: '#delete_confirmation',
+      cssclass: 'overlay-compromisso',
+      noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
+      redirect: location.href,
+      closeselector: '[name="form.button.Cancel"]',
+      width:'50%'
+    });
   }
 }
