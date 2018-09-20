2de9469fcc0c42b35ee3796d09d4cadb25c4f742
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-09-20 15:36:40 -0300

    Add back edit and delete overlays

--- webpack/app/js/agenda.js
+++ webpack/app/js/agenda.js
@@ -10,8 +10,35 @@ export default class AgendaView {
     this.datepicker = new DatePicker(container);
     this.$appointments = this.$('.list-compromissos');
     this.editable = container.dataset.editable === 'True';
+    if (this.editable) {
+      this.makeOverlayEdit();
+    }
   }
   $(selector) {
     return $(selector, this.container.parentElement);
+  }
+  makeOverlayEdit(){
+    for (let item of $('.item-compromisso-wrapper')) {
+      $('.editar_compromisso', item).prepOverlay({
+        subtype: 'ajax',
+        filter: common_content_filter,
+        formselector: '#form',
+        cssclass: 'overlay-compromisso',
+        noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
+        redirect: location.href,
+        closeselector: '[name="form.buttons.cancel"]',
+        width:'50%'
+      });
+      $('.remover_compromisso', item).prepOverlay({
+        subtype: 'ajax',
+        filter: common_content_filter,
+        formselector: '#delete_confirmation',
+        cssclass: 'overlay-compromisso',
+        noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
+        redirect: location.href,
+        closeselector: '[name="form.button.Cancel"]',
+        width:'50%'
+      });
+    }
   }
 }
