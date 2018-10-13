ffb7a8270ed91f911d725f6000672da82cecaaa1
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-10-13 11:31:59 -0300

    Code review

--- src/brasil/gov/agenda/tests/test_upgrades.py
+++ src/brasil/gov/agenda/tests/test_upgrades.py
@@ -56,16 +56,43 @@ class to4100TestCase(UpgradeTestCaseBase):
         self.assertIsNotNone(step)
 
         # simulate state on previous version
+        from brasil.gov.agenda.upgrades.v4100 import SWIPER_CSS
+        from brasil.gov.agenda.upgrades.v4100 import SWIPER_JS
+        from brasil.gov.agenda.upgrades.v4100 import NEW_CSS
         from brasil.gov.agenda.upgrades.v4100 import OLD_CSS
+        from brasil.gov.agenda.upgrades.v4100 import NEW_JS
 
         css_tool = api.portal.get_tool('portal_css')
-        css_tool.registerResource(OLD_CSS)
-        self.assertIn(OLD_CSS, css_tool.getResourceIds())
+        css_tool.unregisterResource(SWIPER_CSS)
+        css_tool.getResource(NEW_CSS).setCompression('safe')
+        css_tool.renameResource(NEW_CSS, OLD_CSS)
+
+        ids = css_tool.getResourceIds()
+        self.assertNotIn(SWIPER_CSS, ids)
+        self.assertNotIn(NEW_CSS, ids)
+        self.assertIn(OLD_CSS, ids)
+        self.assertEqual(css_tool.getResource(OLD_CSS).getCompression(), 'safe')
+
+        js_tool = api.portal.get_tool('portal_javascripts')
+        js_tool.unregisterResource(SWIPER_JS)
+        js_tool.unregisterResource(NEW_JS)
+
+        ids = js_tool.getResourceIds()
+        self.assertNotIn(SWIPER_JS, ids)
+        self.assertNotIn(NEW_JS, ids)
 
         # run the upgrade step to validate the update
         self.execute_upgrade_step(step)
 
-        self.assertNotIn(OLD_CSS, css_tool.getResourceIds())
+        ids = css_tool.getResourceIds()
+        self.assertEqual(SWIPER_CSS, ids[0])
+        self.assertNotIn(OLD_CSS, ids)
+        self.assertIn(NEW_CSS, ids)
+        self.assertEqual(css_tool.getResource(NEW_CSS).getCompression(), 'none')
+
+        ids = js_tool.getResourceIds()
+        self.assertEqual(SWIPER_JS, ids[0])
+        self.assertIn(NEW_JS, ids)
 
 
 class to4101TestCase(UpgradeTestCaseBase):
--- src/brasil/gov/agenda/upgrades/v4100/__init__.py
+++ src/brasil/gov/agenda/upgrades/v4100/__init__.py
@@ -3,12 +3,30 @@ from brasil.gov.agenda.logger import logger
 from plone import api
 
 
+SWIPER_CSS = '//cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.6/css/swiper.min.css'
+SWIPER_JS = '//cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.6/js/swiper.min.js'
 OLD_CSS = '++resource++brasil.gov.agenda/agenda.css'
+NEW_CSS = '++resource++brasil.gov.agenda/brasilgovagenda.css'
+NEW_JS = '++resource++brasil.gov.agenda/brasilgovagenda.js'
+
+
+def add_resource(tool, resource_id):
+    if resource_id in tool.getResourceIds():
+        return
+    tool.registerResource(resource_id)
+    tool.getResource(resource_id).setCompression('none')
 
 
 def fix_resources_references(setup_tool):
     """Fix resource references."""
     css_tool = api.portal.get_tool('portal_css')
+    add_resource(css_tool, SWIPER_CSS)
+    css_tool.moveResourceBefore(SWIPER_CSS, '*')
     if OLD_CSS in css_tool.getResourceIds():
-        css_tool.unregisterResource(OLD_CSS)
+        css_tool.getResource(OLD_CSS).setCompression('none')
+        css_tool.renameResource(OLD_CSS, NEW_CSS)
+    js_tool = api.portal.get_tool('portal_javascripts')
+    add_resource(js_tool, SWIPER_JS)
+    js_tool.moveResourceBefore(SWIPER_JS, '*')
+    add_resource(js_tool, NEW_JS)
     logger.info('Updated static resource references.')
