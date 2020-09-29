5bac8d97d9277e77e95a96af832162f390d0b201
idgserpro <idg@serpro.gov.br>
2020-09-29 12:02:51 -0300

    Back to development: 2.0b4
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0b3'
+version = '2.0b4.dev0'
 description = 'Agenda de membros do Governo Brasileiro'
 long_description = (
     open('README.rst').read() + '\n' +
